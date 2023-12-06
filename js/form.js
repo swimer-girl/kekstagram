7497a717b7a12bddc26846a79c11db5beaff6a58
swimer-girl <swimermail@gmail.com>
2023-12-06 19:03:53 +1000

    правда или действие ч1

--- js/form.js
+++ js/form.js
@@ -5,6 +5,30 @@ const overlay = document.querySelector('.img-upload__overlay');
 const bodyElement = document.querySelector('body');
 const fileField = document.querySelector('#upload-file');
 const cancelButton = document.querySelector('#upload-cancel');
+const hashtagField = document.querySelector('.text__hashtags');
+const commentField = document.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__element',
+  errorTextParent: 'img-upload__element',
+  errorTextClass: 'img-upload__error',
+});
+// Валидатор для хэштегов
+const validateHashtags = (value) => {
+  const hashtagsArray = value.split(' ');
+  for (const hashtag of hashtagsArray) {
+    // Проверка каждого хэш-тега на соответствие ограничениям
+    if (!/^#[A-Za-z0-9]{1,19}$/.test(hashtag)) {
+      return false;
+    }
+  }
+  return true;
+};
+pristine.addValidator(
+  hashtagField, 
+  validateHashtags, 
+  false, 
+  'Хэш-тег должен начинаться с символа # и содержать от 1 до 20 букв или цифр, не более 5 тегов, разделенных пробелами');
 
 // Функция скрытия формы отображения окна изображения
 const hideModal = () => {
@@ -19,6 +43,19 @@ function onEscKeyDown(evt) {
     hideModal();
   }
 }
+// Обработчик события нажатия клавиши для хэш-тега и комментария
+const handleKeyDown = (evt) => {
+  if (evt.key === 'Escape') {
+    // Проверка, в каком поле находится фокус
+    if (evt.target === hashtagField || evt.target === commentField) {
+      evt.stopPropagation();
+    }
+  }
+};
+
+// Назначение обработчиков событий
+hashtagField.addEventListener('keydown', handleKeyDown);
+commentField.addEventListener('keydown', handleKeyDown);
 
 const onCancelButtonClick = () => {
   hideModal();
@@ -33,6 +70,14 @@ const showModal = () => {
 // Обработчик события изменения значения элемента #upload-file
 fileField.addEventListener('change', showModal);
 cancelButton.addEventListener('click', onCancelButtonClick);
+// Событие отправки формы
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const isValid = pristine.validate();
 
-
-export {showModal};
+  if (isValid) {
+    console.log('Форма валидна. Данные могут быть отправлены.');
+  } else {
+    console.log('Форма содержит неверные значения. Пожалуйста, исправьте ошибки.');
+  }
+});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { getPictures } from './data.js';
 import { renderPictures } from './picture.js';
-import { showModal} from './form.js';
+import './form.js';
 
 renderPictures(getPictures());
-showModal();
+//showModal();
