197b6adb99e472c7361d84681a7268eafccce4d0
swimer-girl <swimermail@gmail.com>
2024-02-09 21:32:18 +1000

    ошибка получения данных с сервера

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 const getData = async (onSuccess, onFail) => {
   try {
-    const response = await fetch ('https://25.javascript.pages.academy/kekstagram/data');
+    const response = await fetch ('https://cors-anywhere.herokuapp.com/https://25.javascript.pages.academy/kekstagram/data');
 
     if (!response.ok) {
       throw new Error('Не удалось загрузить фотографии');
