6a27593ab88d16c22057be1e104b8555daa617bb
swimer-girl <swimermail@gmail.com>
2024-02-09 22:29:27 +1000

    test

--- js/api.js
+++ js/api.js
@@ -15,7 +15,7 @@ const getData = async (onSuccess, onFail) => {
 const sendData = async (onSuccess, onFail, body) => {
   try {
     const response = await fetch(
-      'https://cors-proxy.fringe.zone/https://25.javascript.pages.academy/kekstagram',
+      'https://25.javascript.pages.academy/kekstagram/data',
       {
         method: 'POST',
         body,
